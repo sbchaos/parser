dc25ade5e174828cad00d59ad0a7f5a4b36a254d
Sandeep Bhardwaj <bsandeep@thoughtworks.com>
2017-10-14 20:31:12 +0530

    Add more parsers

--- src/JSON/Parser/Parser.hs
+++ src/JSON/Parser/Parser.hs
@@ -2,14 +2,36 @@ module JSON.Parser.Parser where
 
 import Text.ParserCombinators.Parsec
 
+-- | Whitespace removing parser
 ws :: Parser String
 ws = many (oneOf " \t\n")
 
 lexme :: Parser a -> Parser a
 lexme p = ws *> p
 
+-- | null parser
+parseNull :: Parser String
+parseNull = string "null"
+
+-- | The boolean parser
 matchTrue :: Parser Bool
 matchTrue = string "true" *> pure True
 
 matchFalse :: Parser Bool
 matchFalse = string "false" *> pure False
+
+-- The alternative between true and false
+bool :: Parser Bool
+bool = lexme (matchTrue <|> matchFalse)
+
+
+-- | The string parser
+strings :: Parser String
+strings = char '"' *> many (noneOf ['"']) <* char '"'
+
+
+-- | The number parser
+numbers :: Parser Float
+numbers = do
+    digits <- many (digit <|> oneOf ".-")
+    return (read digits)
--- test/JSON/Parser/ParserSpec.hs
+++ test/JSON/Parser/ParserSpec.hs
@@ -16,16 +16,41 @@ main = hspec spec
 spec :: Spec
 spec =
   describe "Parser" $ do
+
     describe "matchTrue" $ do
       it "matches the string true" $
         parse matchTrue "test" "true" `shouldBe` Right True
       it "errors whin string is not true" $
         isLeft (parse matchTrue "test" "blue") `shouldBe` True
+
     describe "mathFalse" $ do
       it "matches the string false" $
-       parse matchFalse "test" "false" `shouldBe` Right False
+        parse matchFalse "test" "false" `shouldBe` Right False
       it "errors whin string is not true" $
-       isLeft (parse matchFalse "test" "blue") `shouldBe` True
+        isLeft (parse matchFalse "test" "blue") `shouldBe` True
+
+    describe "bool" $ do
+      it "matches true" $
+        parse bool "test" "true\t" `shouldBe` Right True
+      it "matches false" $
+        parse bool "test" "false " `shouldBe` Right False
+
     describe "lexme combinator" $
       it "removes leading and trailing whitespace" $
-        parse (lexme matchTrue) "description" "\t  true \n" `shouldBe` Right True
+        parse (lexme matchTrue) "test" "\t  true \n" `shouldBe` Right True
+
+    describe "strings" $ do
+      it "extrcts string from text" $
+        parse strings "test" "\"Hello\"" `shouldBe` Right "Hello"
+      it "extracts strings from text" $
+        parse strings "test" "\"Hello World\"" `shouldBe` Right "Hello World"
+
+    describe "numbers" $ do
+      it "extracts the number" $
+        parse numbers "test" "1234" `shouldBe` Right 1234
+      it "extracts the negative numbers" $
+        parse numbers "test" "-123" `shouldBe` Right (-123.0)
+
+    describe "parseNull" $
+      it "matches null value" $
+        parse parseNull "test" "null" `shouldBe` Right "null"
